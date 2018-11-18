202ceef0c906dc94d233bf644c32f741f060e7cd
Dhruva Agarwal <dhruva.agarwal15@gmail.com>
2018-11-18 19:52:28 +0530

    Next Commit
    Signed-off-by: Avi Gupta <avi.gupta@st.niituniversity.in>

--- app/src/main/java/com/progtools/eventmanagement/MainActivity.java
+++ app/src/main/java/com/progtools/eventmanagement/MainActivity.java
@@ -1,15 +1,25 @@
- package com.progtools.eventmanagement;
+package com.progtools.eventmanagement;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
- public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth firebaseAuth;
+
+    private TextView textViewUserEmail;
+    private Button buttonLogout;
 
     private RecyclerView recyclerView;
     String []Items={"item 0", "item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9"};
@@ -20,11 +30,36 @@ import com.google.firebase.FirebaseApp;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView=(RecyclerView) findViewById(R.id.recyclerview);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, Login_Activity.class));
+        }
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        textViewUserEmail = findViewById(R.id.textViewUserEmail);
+
+        textViewUserEmail.setText("Welcome" + user.getEmail());
+        buttonLogout = findViewById(R.id.buttonLogout);
+
+        buttonLogout.setOnClickListener(this);
+
+        recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new Adapter(this, Items ));
 
  //       mfirestore= FirebaseFirestore.getInstance();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonLogout) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, Login_Activity.class));
+        }
     }
 
     @Override
